96453700aafdfb77c0c5f021f165932c26aab0a7
HEMSDEV1 <HEMSDEV1@krish4dev.org>
2025-03-13 08:55:52 +0530

    Sample kafka examples

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+class Consumer
+{
+
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            // User-specific properties that you must set
+            BootstrapServers = "<BOOTSTRAP_SERVERS>",
+            SaslUsername = "<API_KEY>",
+            SaslPassword = "<API_SECRET>",
+
+            // Fixed properties
+            SecurityProtocol = SecurityProtocol.SaslSsl,
+            SaslMechanism = SaslMechanism.Plain,
+            GroupId = "kafka-dotnet-getting-started",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        const string topic = "TestTopic";
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true;
+            cts.Cancel();
+        };
+
+        using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+        {
+            consumer.Subscribe(topic);
+            try
+            {
+                while (true)
+                {
+                    var cr = consumer.Consume(cts.Token);
+                    //Console.WriteLine($"Consumed event from topic {topic}: key = {cr.Message.Key,-10} value = {cr.Message.Value}");
+                    Console.WriteLine($"Consumed {cr.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Ctrl-C was pressed.
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+const string topic = "TestTopic";
+
+var config = new ProducerConfig
+{
+    // User-specific properties that you must set
+    BootstrapServers = "<BOOTSTRAP_SERVERS>",
+    SaslUsername = "<API_KEY>",
+    SaslPassword = "<API_SECRET>",
+
+    // Fixed properties
+    SecurityProtocol = SecurityProtocol.SaslSsl,
+    SaslMechanism = SaslMechanism.Plain,
+    Acks = Acks.All
+};
+
+using (var producer = new ProducerBuilder<string, string>(config).Build())
+{
+    var numProduced = 0;
+    Console.WriteLine("\n\n");
+    Console.WriteLine("Note: Type N for exit the application\n\n");
+
+    while (true)
+    {
+        Console.WriteLine("Enter Message to send");
+        string message = Console.ReadLine();
+        if (message == "N" || message == "n")
+        {
+            Console.WriteLine("You entered N to exit application");
+            producer.Flush(TimeSpan.FromSeconds(10));
+            Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            break;
+        }
+
+        producer.Produce(topic, new Message<string, string> { Key = "Test", Value = message },
+           (deliveryReport) =>
+           {
+               if (deliveryReport.Error.Code != ErrorCode.NoError)
+               {
+                   Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+               }
+               else
+               {
+                   Console.WriteLine($"Produced event to topic {topic}: key = Test value = {message}");
+                   numProduced += 1;
+               }
+           });
+    }
+}
